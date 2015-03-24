1e81c37c7ac33d94b5233c17849df0b1e2040d39
The Don <don@donmail.don>
2015-03-24 17:25:09 +0300

    Ready to push

---
+++ AnalyzerViewer/AnalyzerView.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lucene.Net.Analysis;
+using Lucene.Net.Analysis.Tokenattributes;
+
+namespace AnalyzerViewer
+{
+
+    public abstract class AnalyzerView
+    {
+
+        public abstract string Name { get; }
+
+        //public virtual string GetView(TokenStream tokenStream, out int numberOfTokens)
+        //{
+        //    StringBuilder sb = new StringBuilder();
+
+        //    Token token = tokenStream.Next();
+
+        //    numberOfTokens = 0;
+
+        //    while (token != null)
+        //    {
+        //        numberOfTokens++;
+        //        sb.Append(GetTokenView(token));
+        //        token = tokenStream.Next();
+        //    }
+
+        //    return sb.ToString();
+        //}
+
+        public virtual string GetView(TokenStream tokenStream, out int numberOfAttributes)
+        {
+            StringBuilder sb = new StringBuilder();
+            //instead of CharTermAttribute, use 
+            //ITermAttribute termAttr = tokenStream.GetAttribute<ITermAttribute>(); //try addattribute 
+            //Attribute termAttr = tokenStream.GetAttribute<Attribute>();
+            //Error here. Resolved ITermAttribute is a must!
+
+            ITermAttribute termAttr = tokenStream.GetAttribute<ITermAttribute>(); //try addattribute here
+
+            //Error here
+
+            //http://stackoverflow.com/questions/16274779/get-termattribute-in-tokenstream-lucene-net
+            numberOfAttributes = 0;
+            tokenStream.Reset(); //try out without
+            while (tokenStream.IncrementToken())
+            {
+                numberOfAttributes++;
+                sb.Append(GetAttributeView(termAttr));
+                termAttr = tokenStream.GetAttribute<ITermAttribute>();
+                //string term = termAttr.Term;
+            }
+            tokenStream.End();
+            tokenStream.Dispose();
+            //http://stackoverflow.com/questions/2638200/how-to-get-a-token-from-a-lucene-tokenstream
+            return sb.ToString();
+        }
+
+        //protected abstract string GetTokenView(Token token);
+
+        protected abstract string GetAttributeView(ITermAttribute termAttribute);
+
+    }
+
+    public class TermAnalyzerView : AnalyzerView
+    {
+
+        public override string Name
+        {
+            get { return "Terms"; }
+        }
+
+        //protected override string GetTokenView(Token token) { return "[" + token.TermText() + "]   "; }
+        public override string GetView(TokenStream tokenStream, out int numberOfAttributes) { return base.GetView(tokenStream, out numberOfAttributes); }
+
+        protected override string GetAttributeView(ITermAttribute termAttribute) { return "[" + termAttribute.Term + "]   "; }
+
+    }
+
+    /// <summary>
+    ///     Not implemented!
+    /// </summary>
+    public class TermWithOffsetsView : AnalyzerView
+    {
+
+        public override string Name
+        {
+            get { return "Terms With Offsets"; }
+        }
+
+        //protected override string GetTokenView(Token token)
+        //{
+
+        //    return token.TermText() + "   Start: " + token.StartOffset().ToString().PadLeft(5) + "  End: "
+        //           + token.EndOffset().ToString().PadLeft(5) + "\r\n";
+        //}
+
+        //protected override string GetAttributeView(TermAttribute termAttribute) { return termAttribute.Term + "   Start: " + Lucene.Net.Analysis.Tokenattributes.IOffsetAttribut}
+        protected override string GetAttributeView(ITermAttribute termAttribute) { throw new NotImplementedException(); }
+
+    }
+
+    public class TermFrequencies : AnalyzerView
+    {
+
+        public override string Name
+        {
+            get { return "Term Frequencies"; }
+        }
+
+        private Dictionary<string, int> termDictionary = new Dictionary<string, int>();
+
+        public override string GetView(TokenStream tokenStream, out int numberOfAttributes)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            //Token token = tokenStream.Next();
+            ITermAttribute termAttribute = tokenStream.GetAttribute<ITermAttribute>();
+
+            numberOfAttributes = 0;
+
+            while (tokenStream.IncrementToken())
+            {
+                numberOfAttributes++;
+
+                if (termDictionary.Keys.Contains(termAttribute.Term))
+                {
+                    termDictionary[termAttribute.Term] = termDictionary[termAttribute.Term] + 1;
+                }
+                else
+                {
+                    termDictionary.Add(termAttribute.Term, 1);
+                }
+
+                termAttribute = tokenStream.GetAttribute<ITermAttribute>();
+
+                //if (termDictionary.Keys.Contains(token.TermText()))
+                //    termDictionary[token.TermText()] = termDictionary[token.TermText()] + 1;
+                //else
+                //    termDictionary.Add(token.TermText(), 1);
+
+                //token = tokenStream.Next();
+            }
+
+            foreach (var item in termDictionary.OrderBy(x => x.Key))
+            {
+                sb.Append(item.Key + " [" + item.Value + "]   ");
+            }
+
+            termDictionary.Clear();
+
+            return sb.ToString();
+        }
+
+        protected override string GetAttributeView(ITermAttribute termAttribute) { throw new NotImplementedException(); }
+
+        //protected override string GetTokenView(Token token) { throw new NotImplementedException(); }
+    }
+
+}
---
+++ AnalyzerViewer/MainForm.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using Lucene.Net.Analysis;
+using Lucene.Net.Analysis.Ru;
+using Lucene.Net.Analysis.Snowball;
+using Lucene.Net.Analysis.Standard;
+using Version = Lucene.Net.Util.Version;
+
+namespace AnalyzerViewer
+{
+
+    public partial class MainForm : Form
+    {
+
+        public BindingList<AnalyzerInfo> AnalyzerInfoList = new BindingList<AnalyzerInfo>();
+        public BindingList<AnalyzerView> AnalyzerViewsList = new BindingList<AnalyzerView>();
+        //Without Bindinglist, result in tbOutputText loose their original sort and display in alphabetical order  
+        //public List<AnalyzerInfo> AnalyzerInfoList = new List<AnalyzerInfo>();
+        //public List<AnalyzerView> AnalyzerViewsList = new List<AnalyzerView>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            AnalyzerInfoList.Add(new AnalyzerInfo("Keyword Analyzer", "\"Tokenizes\" the entire stream as a single token.",
+                new KeywordAnalyzer()));
+            AnalyzerInfoList.Add(new AnalyzerInfo("Whitespace Analyzer", "An Analyzer that uses WhitespaceTokenizer.",
+                new WhitespaceAnalyzer()));
+            AnalyzerInfoList.Add(new AnalyzerInfo("Stop Analyzer", "Filters LetterTokenizer with LowerCaseFilter and StopFilter.",
+                new StopAnalyzer(Version.LUCENE_30)));
+            AnalyzerInfoList.Add(new AnalyzerInfo("Simple Analyzer", "An Analyzer that filters LetterTokenizer with LowerCaseFilter.",
+                new SimpleAnalyzer()));
+            AnalyzerInfoList.Add(new AnalyzerInfo("Standard Analyzer",
+                "Filters StandardTokenizer with StandardFilter, LowerCaseFilter and StopFilter, using a list of English stop words.",
+                new StandardAnalyzer(Version.LUCENE_30)));
+            AnalyzerInfoList.Add(new AnalyzerInfo("Russian Analyzer",
+                "RussianLetterTokenizer, RussianLowerCaseFilter, StopFilter (custom stop list), RussianStemFilter.",
+                new RussianAnalyzer(Version.LUCENE_30)));
+            AnalyzerInfoList.Add(new AnalyzerInfo("Russian Snowball Analyzer",
+                "StandardTokenizer, StandardFilter, LowerCaseFilter, [StopFilter] SnowballFilter.",
+                new SnowballAnalyzer(Version.LUCENE_30, "Russian")));
+
+            AnalyzerViewsList.Add(new TermAnalyzerView());
+            //AnalyzerViewsList.Add(new TermWithOffsetsView());
+            AnalyzerViewsList.Add(new TermFrequencies());
+
+            tbDescription.DataBindings.Add(new Binding("Text", AnalyzerInfoList, "Description"));
+
+            cbAnalysers.DisplayMember = "Name";
+            cbAnalysers.ValueMember = "LuceneAnalyzer";
+            cbAnalysers.DataSource = AnalyzerInfoList;
+
+            cbViews.DisplayMember = "Name";
+            cbViews.DataSource = AnalyzerViewsList;
+
+            cbAnalysers.SelectedIndex = 0;
+            cbViews.SelectedIndex = 0;
+
+            cbAnalysers.SelectedValueChanged += new EventHandler(cbAnalysers_SelectedValueChanged);
+            cbViews.SelectedValueChanged += new EventHandler(cbViews_SelectedValueChanged);
+            tbSourceText.TextChanged += new EventHandler(tbSourceText_TextChanged);
+
+            tbSourceText.Text = "The quick brown fox jumped over the lazy dog.";
+            AnalyzeText();
+        }
+
+        private void cbViews_SelectedValueChanged(object sender, EventArgs e) { AnalyzeText(); }
+
+        private void tbSourceText_TextChanged(object sender, EventArgs e) { AnalyzeText(); }
+
+        private void cbAnalysers_SelectedValueChanged(object sender, EventArgs e) { AnalyzeText(); }
+
+        public void AnalyzeText()
+        {
+            Analyzer analyzer = cbAnalysers.SelectedValue as Analyzer;
+
+            int termCounter = 0;
+
+            if (analyzer != null)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                AnalyzerView view = cbViews.SelectedValue as AnalyzerView;
+
+                StringReader stringReader = new StringReader(tbSourceText.Text);
+
+                TokenStream tokenStream = analyzer.TokenStream("defaultFieldName", stringReader);
+
+                tbOutputText.Text = view.GetView(tokenStream, out termCounter).Trim();
+            }
+
+            lblStats.Text = string.Format("Total of {0} Term(s) Found.", termCounter);
+        }
+
+        private void Form1_Load(object sender, EventArgs e) { }
+
+    }
+
+}
